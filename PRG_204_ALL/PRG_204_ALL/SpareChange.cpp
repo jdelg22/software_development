995ea2b3ab5c05de1c6989c086f4d21ac9ad8e18
jdelg22 <jondelgado_06@yahoo.com>
2017-11-29 18:47:37 -0800

    Chapter 1, pb. 12
    Chapter 1, pb. 12

---
+++ PRG_204_ALL/PRG_204_ALL/SpareChange.cpp
@@ -0,0 +1,9 @@
+
+#include <cstdio>
+
+void SpareChange( void )
+{
+	double amount = 7.36;
+	printf( "In my pocket, I have %f dollars.", amount );
+	return;
+}
---
+++ PRG_204_ALL/PRG_204_ALL/main.cpp
@@ -0,0 +1,11 @@
+
+
+#define string char*
+
+void SpareChange( void );
+
+int main( int argc, string argv[  ] )
+{
+	SpareChange(  );
+	return 0;
+}
