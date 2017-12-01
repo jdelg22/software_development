d2895821ebbde76ee60696ec2d108164d43edf3f
jdelg22 <jondelgado_06@yahoo.com>
2017-11-30 19:58:13 -0800

    Ch 2 pb 4
    Ch 2 pb 4

---
+++ PRG_204_ALL/PRG_204_ALL/Floor.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+void Net_weekly_pay( void )
+{
+	double hours = 0
+	double gross_income = 0;
+	double net_income = 0;
+	printf( "How many hours did it take to install the hardwood floor?\r\n" );
+	scanf( "%lf", &hours);
+	gross_income = hours * hours;
+	printf( "what is the withholding percentage?\r\n" );
+	scanf( "%lf ", &withholding_percentage );
+	net_income = gross_income * (100 - withholding_percentage) / 100;
+	printf( "Your gross pay is %lf.", gross_income );
+	printf( "Your net weekly pay is %lf.", net_income );
+
+	return;
+}
